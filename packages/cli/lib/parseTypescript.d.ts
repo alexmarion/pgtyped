0069203486294c268f018d2f1620e96b50f1de31
Alex Marion <alex@gotab.io>
2024-09-04 15:30:54 -1000

    removed lib

--- packages/cli/lib/parseTypescript.d.ts
+++
@@ -1,9 +0,0 @@
-import { ParseEvent, TSQueryAST } from '@pgtyped/parser';
-import ts from 'typescript';
-import { TransformConfig } from './config.js';
-export type TSParseResult = {
-    queries: TSQueryAST[];
-    events: ParseEvent[];
-};
-export declare function parseFile(sourceFile: ts.SourceFile, transformConfig: TransformConfig | undefined): TSParseResult;
-export declare const parseCode: (fileContent: string, fileName?: string, transformConfig?: TransformConfig) => TSParseResult;
